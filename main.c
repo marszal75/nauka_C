8f5aff7be1389b6526e2ee1e5037bf1a863db96e
marszal75 <marszal75@gmail.com>
2014-12-28 12:17:59 +0100

    nauka 2

--- main.c
+++ main.c
@@ -1,17 +1,23 @@
-/* 
- * File:   main.c
- * Author: rm
- 
- */
-
+//Tabela wielowymiarowa
 #include <stdio.h>
-#include <stdlib.h>
-
-/*
- * 
- */
-int main(int argc, char** argv) {
-
-    return (EXIT_SUCCESS);
+float Tablica[4][5];
+int licz1, licz2;
+int main()
+{
+    for (licz1 = 0; licz1 < 4; licz1++)
+    {
+        for (licz2 = 0; licz2 < 5; licz2++)
+        {
+            printf("\n\n Wpisz srednią ocen %d-tego ucznia %d-tej klasy: ", licz1+1, licz2+1);
+            scanf("%f", &Tablica[licz1][licz2]);
+        }    
+    }
+for (licz1 = 0; licz1 < 4; licz1++)
+{
+    for (licz2 = 0; licz2 < 5; licz2++)
+    {
+        printf("średnia ocen %d-ego ucznia %d-tej klasy wynosi: %.2f\n", licz1+1, licz2+1, Tablica[licz1][licz2]);
+    }
+}    
+            return 0;
 }
-
