ca2d7b263660fcc91f4ec4371d8d16ae2220db18
marszal75 <marszal75@gmail.com>
2014-12-28 15:40:00 +0100

    Pointer lite

--- main.c
+++ main.c
@@ -1,22 +1,14 @@
 #include <stdio.h>
-
-int Tablica[2][12] = {{31,29,31,30,31,30,31,31,30,31,30,31}, {31,28,31,30,31,30,31,31,30,31,30,31}};
-int a, b;
+int Ola;
+int *Zbych;
 
 int main()
 {
-  for (a=0; a<2; a++)
-  {
-      for (b=0; b<12; b++)
-      { 
-          if (a==0)
-          {
-             printf("Miesiąc %d, roku przestępnego ma %d dni\n", b+1, Tablica[a][b] ); 
-          }
-          else
-          {
-             printf("Miesiąc %d, roku nieprzestępnego ma %d dni\n", b+1, Tablica[a][b] ); 
-          }
-      }
-  }
+    Ola = 180;
+    Zbych = &Ola;
+    printf("Wzrost Oli: %d\n", Ola);
+    printf("Wzrost Oli wg Zbycha: %d\n", *Zbych);
+    printf("Adres Oli: %p\n", &Ola);
+    printf("Adres Oli wg Zbycha: %p\n", Zbych);
+    return 0;
 }
