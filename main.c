84ebd434c20d70b5ca18892255559b092f399904
marszal75 <marszal75@gmail.com>
2014-12-28 16:14:13 +0100

    Zliczanie maksimum z tablicy

--- main.c
+++ main.c
@@ -1,14 +1,33 @@
+//Program zliczający największą wartość w tablicy
+
 #include <stdio.h>
-int Ola;
-int *Zbych;
+#define rozm 10
+int tab[rozm];
+int licznik;
+int maksimum(int x[], int y);
 
 int main()
 {
-    Ola = 180;
-    Zbych = &Ola;
-    printf("Wzrost Oli: %d\n", Ola);
-    printf("Wzrost Oli wg Zbycha: %d\n", *Zbych);
-    printf("Adres Oli: %p\n", &Ola);
-    printf("Adres Oli wg Zbycha: %p\n", Zbych);
+    for (licznik = 0; licznik < rozm; licznik++)
+    {
+        printf("wprowadź wartości tablicy: ");
+        scanf("%d", &tab[licznik]);
+    }
+    for (licznik = 0; licznik < rozm; licznik++)
+    {
+        printf("%d, ", tab[licznik]);
+    }
+    printf("\nNajwiększa wartość to: %d", maksimum(tab, rozm));
     return 0;
+}
+int maksimum( int x[], int y )
+{
+    int licz;
+    int maksim = 0;
+    for (licz = 0; licz < y; licz++)
+    {
+        if (x[licz] > maksim)
+            maksim = x[licz];
+    }
+    return maksim;
 }
