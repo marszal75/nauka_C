92391fc32147f96aac7840dccdbf649a02240d5a
marszal75 <marszal75@gmail.com>
2015-01-14 00:06:47 +0100

    Wskaźniki same

--- main.c
+++ main.c
@@ -1,42 +1,18 @@
-//Program zliczający dwie tablice o tym samym rozmiarze
+// Program ze wskaźnikami na tablicę
 
 #include <stdio.h>
-#define rozm 5
-int i, Tab1[rozm], Tab2[rozm];
-int suma1, suma2, sumasum;
+int Ola;
+int *Zbych;
+
 int main()
 {
-    // Tablica 1
-    for (i=0; i<rozm; i++)
-    {
-        printf("Podaj wartości tablicy: \n");
-        scanf("%d", &Tab1[i]);
-        suma1 += Tab1[i];
-    }
-    printf("Wartości tablicy to: ");
-    for (i=0; i<rozm; i++)
+    Ola = 180;
+    Zbych = &Ola;
     
-        printf(" %d,", Tab1[i]);
+    printf("normalna  OLA to %d\n",Ola);
+    printf("nienormalna OLA to %d\n",*Zbych);
+        printf("normalna  OLA to %d\n",&Ola);
+    printf("nienormalna OLA to %d\n",Zbych);
     
-    printf("\n");
-    // Tablica 2
-    for (i=0; i<rozm; i++)
-    {
-        printf("Podaj wartości drugiej tablicy: \n");
-        scanf("%d", &Tab2[i]);
-        suma2 += Tab2[i];
-    }
-    printf("Wartości drugiej tablicy to: ");
-    for (i=0; i<rozm; i++)
-    
-        printf(" %d,", Tab2[i]);
-    
-    printf("\n");
-    printf("suma elementów tablicy 1 = %d\n", suma1);
-    printf("suma elementów tablicy 2 = %d\n", suma2);
-    sumasum = suma1 + suma2;
-    printf(" Suma sum = %d", sumasum);
     return 0;
-        
-    
 }
