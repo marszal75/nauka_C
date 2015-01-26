ed022451925065075e4c4428342f00bc3a921df2
marszal75 <marszal75@gmail.com>
2015-01-26 19:46:32 +0100

    Porównanie łańcuchów znaków

--- main.c
+++ main.c
@@ -1,18 +1,25 @@
-// Program ze wskaźnikami na tablicę
-
+//porównanie dwóch łańcuchów znaków
 #include <stdio.h>
-int Ola;
-int *Zbych;
+#include <stdlib.h>
 
+char bufor1[100], bufor2[100];
+char *funkcja(char x[], char y[]);
 int main()
 {
-    Ola = 180;
-    Zbych = &Ola;
-    
-    printf("normalna  OLA to %d\n",Ola);
-    printf("nienormalna OLA to %d\n",*Zbych);
-        printf("normalna  OLA to %d\n",&Ola);
-    printf("nienormalna OLA to %d\n",Zbych);
-    
+    puts("wpisz pierwszy tekst: ");
+    gets(bufor1);
+    puts("wpisz drugi tekst: ");
+    gets(bufor2);
+    printf("Dłuższy łańcuch znaków to: %s\n",funkcja(bufor1, bufor2));
     return 0;
+}
+char *funkcja(char x[], char y[])
+{
+    size_t a, b;
+    a = strlen(x);
+    b = strlen(y);
+    if(a > b )
+        return x;
+    else
+        return y;
 }
