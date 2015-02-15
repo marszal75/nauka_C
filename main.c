b6e650fc4f801584fb051fe826de5ba75accfc49
marszal75 <marszal75@gmail.com>
2015-02-15 21:49:18 +0100

    Wątki 1

--- main.c
+++ main.c
@@ -1,25 +1,38 @@
-//porównanie dwóch łańcuchów znaków
+/******************************************************************************
+* FILE: hello.c
+* DESCRIPTION:
+*   A "hello world" Pthreads program.  Demonstrates thread creation and
+*   termination.
+* AUTHOR: Blaise Barney
+* LAST REVISED: 08/09/11
+******************************************************************************/
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#define NUM_THREADS	5
 
-char bufor1[100], bufor2[100];
-char *funkcja(char x[], char y[]);
-int main()
+void *PrintHello(void *threadid)
 {
-    puts("wpisz pierwszy tekst: ");
-    gets(bufor1);
-    puts("wpisz drugi tekst: ");
-    gets(bufor2);
-    printf("Dłuższy łańcuch znaków to: %s\n",funkcja(bufor1, bufor2));
-    return 0;
+   long tid;
+   tid = (long)threadid;
+   printf("Hello World! It's me, thread #%ld!\n", tid);
+   pthread_exit(NULL);
 }
-char *funkcja(char x[], char y[])
+
+int main(int argc, char *argv[])
 {
-    size_t a, b;
-    a = strlen(x);
-    b = strlen(y);
-    if(a > b )
-        return x;
-    else
-        return y;
+   pthread_t threads[NUM_THREADS];
+   int rc;
+   long t;
+   for(t=0;t<NUM_THREADS;t++){
+     printf("In main: creating thread %ld\n", t);
+     rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
+     if (rc){
+       printf("ERROR; return code from pthread_create() is %d\n", rc);
+       exit(-1);
+       }
+     }
+
+   /* Last thing that main() should do */
+   pthread_exit(NULL);
 }
